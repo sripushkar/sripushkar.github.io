9f0af8bd4e96c63172e009b46830c3202d57ca88
Sri Julapally <sripushkar@gmail.com>
2020-08-10 02:41:21 -0400

    flashing

---
+++ scripts.js
@@ -0,0 +1,17 @@
+var images = ["url('img/wave1.svg')", "url('img/wave2.svg')"];
+$(function() {
+  var i = 0;
+  $("#particles-js").css("background-image", images[i]);
+  setInterval(function() {
+    if(i == 0) {
+      i = 1;
+    }
+    else if (i == 1) {
+      i = 0;
+    }
+    $("#particles-js").fadeOut("slow", function() {
+      $(this).css("background-image", images[i]);
+      $(this).fadeIn("slow");
+    });
+  }, 3000);
+});
