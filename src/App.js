7847459d7ed68f595bab5498497b12d09e3b0e97
Sri Julapally <sripushkar@gmail.com>
2020-10-07 16:08:03 -0400

    added react modules

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Container } from '@material-ui/core'
+import './Bulma.sass'
+import { starsConfig } from "./data/particlesConfig.js"
+import Particles from 'react-particles-js';
 
 class App extends Component {
   render() {
+    const particlesStyles = {
+      position:"absolute",
+      left:"0",
+      top:"0",
+      width:"100%",
+      height:"100%"
+    }
+    const bgStyles = {
+      backgroundColor: "black", 
+      width: "100%", 
+      height: "100%"      
+    }
     return (
-      <Container>
-        
-      </Container>
+      <div style={bgStyles}>
+        <Particles params={starsConfig} style={particlesStyles}/>
+      </div>
     );
   }
 }
