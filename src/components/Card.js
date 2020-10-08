c61bd815c85297076ff478b22d836908647ab19b
Sri Julapally <sripushkar@gmail.com>
2020-10-08 11:17:21 -0400

    added card

---
+++ src/components/Card.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+
+export default class Card extends Component {
+    render() {
+        const spacing = {
+            marginBottom: '10%'
+        }
+          
+        return (    
+                <div className='container has-text-centered'> 
+                    <div className='columns is-mobile is-centered'> 
+                    <div className='column is-5'> 
+                        <div className="card"> 
+                        <div className="card-content"> 
+                            <p className='title is-centered' style={spacing}> 
+                                Sripushkar Julapally
+                            </p> 
+                
+                            <p className='subtitle is-centered'> 
+                                Freelance Developer | Software Programmer | Leader
+                            </p> 
+                
+                        </div> 
+                        </div> 
+                    </div> 
+                    </div> 
+                </div>           
+                
+        )
+    }
+}
