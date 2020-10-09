635164cfbee8fbc1011fa3f4bb26db05a78b0834
Sri Julapally <sripushkar@gmail.com>
2020-10-09 11:30:56 -0400

    stars working

--- App.js
+++ App.js
@@ -1,4 +1,18 @@
-particlesJS.load('particles-js', {
+/* -----------------------------------------------
+/* How to use? : Check the GitHub README
+/* ----------------------------------------------- */
+
+/* To load a config file (particles.json) you need to host this demo (MAMP/WAMP/local)... */
+/*
+particlesJS.load('particles-js', 'particles.json', function() {
+  console.log('particles.js loaded - callback');
+});
+*/
+
+/* Otherwise just put the config content (json): */
+
+particlesJS('particles-js',
+{
     "particles": {
       "number": {
         "value": 160,
@@ -107,6 +121,5 @@ particlesJS.load('particles-js', {
       }
     },
     "retina_detect": true
-  }, function() {
-    console.log('callback - particles.js config loaded');
-  });
+  }
+);
